38859441ed4e62e18995928e0c4270234219be5c
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-05-08 14:24:58 +0200

    Implemented div targeting based on letter position

--- javascript.js
+++ javascript.js
@@ -1,12 +1,24 @@
 
 function checkWord() {
-    var word = "hangman";
+    var word = "index";
     var inputletter = document.getElementById('input').value;
+    var inputindex = word.indexOf(inputletter);
     
     if (word.includes(inputletter)) {
+
+    if (inputindex == 0) {
         document.getElementById('letter1').innerHTML = inputletter;
+    } else if (inputindex == 1) {
+        document.getElementById('letter2').innerHTML = inputletter;
+    } else if (inputindex == 2) {
+        document.getElementById('letter3').innerHTML = inputletter;
+    } else if (inputindex == 3) {
+        document.getElementById('letter4').innerHTML = inputletter;
+    } else if (inputindex == 4) {
+        document.getElementById('letter5').innerHTML = inputletter;
     } 
-    
+
+}
 }  
 
 document.getElementById('check').addEventListener('click', checkWord);
