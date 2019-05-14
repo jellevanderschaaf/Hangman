b1a4f5180b8e954a3dc03cd2e02d5f7f73ad97b0
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-05-14 16:31:20 +0200

    Added a score notification including gallow visuals. Only supporting a losing game for the moment.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+var wrongGuess = 1;
+var attemptsLeft = 8;
 var wordlist = [ 
     "seven", 
     "world", 
@@ -55,8 +57,20 @@ function checkWord() {
     } else if (inputindex == 4) {
         document.getElementById('letter5').innerHTML = inputletter;
     } 
+    } else {
+        if (wrongGuess < 9) {
+            wrongGuess += 1;
+        } if (attemptsLeft >= 1) {
+            attemptsLeft -= 1;
+            document.getElementById('scoreNotification').innerHTML = "You have " + attemptsLeft + " attempts left.";
+        } if (attemptsLeft == 1) {
+            document.getElementById('scoreNotification').innerHTML = "You have 1 attempt left";
+        } if (attemptsLeft == 0) {
+            document.getElementById('scoreNotification').innerHTML = "You loose!";
+        }
+        document.getElementById('image').src='hangman' + wrongGuess + '.jpg';
+    }   
 
-}
 }  
 
 document.getElementById('check').addEventListener('click', checkWord);
