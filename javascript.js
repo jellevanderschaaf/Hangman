9f729f9413a32527bff75ccf4b394c0c47917bdf
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-05-15 16:02:51 +0200

    Added win conditions.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,24 @@
+// Global variables
+
 var wrongGuess = 1;
 var attemptsLeft = 8;
+var letter1Guessed = " ";
+var letter2Guessed = " ";
+var letter3Guessed = " ";
+var letter4Guessed = " ";
+var letter5Guessed = " ";
+
 var wordlist = [ 
-    "seven", 
     "world", 
-    "about", 
-    "again", 
+    "about",  
     "heart", 
     "water", 
-    "happy", 
     "sixty", 
     "board", 
     "month", 
     "angel", 
     "death", 
-    "green",
     "music", 
-    "fifty", 
-    "three", 
     "party", 
     "piano",  
     "mouth", 
@@ -24,19 +26,17 @@ var wordlist = [
     "sugar", 
     "amber", 
     "dream", 
-    "apple", 
     "laugh", 
     "tiger", 
     "faith", 
-    "earth", 
-    "river", 
+    "earth",  
     "money", 
     "peace", 
-    "pizza", 
 ];
     
 var word = wordlist[Math.floor(Math.random()*wordlist.length)];
 
+// via inputfield
 
 function checkWord() {
     
@@ -50,14 +50,19 @@ function checkWord() {
 
     if (inputindex == 0) {
         document.getElementById('letter1').innerHTML = inputletter;
+        letter1Guessed = "yes";
     } else if (inputindex == 1) {
         document.getElementById('letter2').innerHTML = inputletter;
+        letter2Guessed = "yes";
     } else if (inputindex == 2) {
         document.getElementById('letter3').innerHTML = inputletter;
+        letter3Guessed = "yes";
     } else if (inputindex == 3) {
         document.getElementById('letter4').innerHTML = inputletter;
+        letter4guessed = "yes";
     } else if (inputindex == 4) {
         document.getElementById('letter5').innerHTML = inputletter;
+        letter5Guessed = "yes";
     } 
     } else {
 
@@ -67,7 +72,7 @@ function checkWord() {
             wrongGuess += 1;
         } if (attemptsLeft >= 1) {
             attemptsLeft -= 1;
-            document.getElementById('scoreNotification').innerHTML = "You have " + attemptsLeft + " attempts left.";
+            document.getElementById('scoreNotification').innerHTML = "You have " + attemptsLeft + " attempts left";
         } if (attemptsLeft == 1) {
             document.getElementById('scoreNotification').innerHTML = "You have 1 attempt left";
         } if (attemptsLeft == 0) {
@@ -80,6 +85,7 @@ function checkWord() {
 
 document.getElementById('check').addEventListener('click', checkWord);
 
+// via clickable buttons
 
 function checkLetter(value) {
 
@@ -93,14 +99,19 @@ function checkLetter(value) {
 
     if (inputindex == 0) {
         document.getElementById('letter1').innerHTML = value;
+        letter1Guessed = "yes";
     } else if (inputindex == 1) {
         document.getElementById('letter2').innerHTML = value;
+        letter2Guessed = "yes";
     } else if (inputindex == 2) {
         document.getElementById('letter3').innerHTML = value;
+        letter3Guessed = "yes";
     } else if (inputindex == 3) {
         document.getElementById('letter4').innerHTML = value;
+        letter4Guessed = "yes";
     } else if (inputindex == 4) {
         document.getElementById('letter5').innerHTML = value;
+        letter5Guessed = "yes";
     } 
     } else {
 
@@ -110,7 +121,7 @@ function checkLetter(value) {
             wrongGuess += 1;
         } if (attemptsLeft >= 1) {
             attemptsLeft -= 1;
-            document.getElementById('scoreNotification').innerHTML = "You have " + attemptsLeft + " attempts left.";
+            document.getElementById('scoreNotification').innerHTML = "You have " + attemptsLeft + " attempts left";
         } if (attemptsLeft == 1) {
             document.getElementById('scoreNotification').innerHTML = "You have 1 attempt left";
         } if (attemptsLeft == 0) {
@@ -118,5 +129,9 @@ function checkLetter(value) {
         }
         document.getElementById('image').src='hangman' + wrongGuess + '.jpg';
     }   
+
+if (letter1Guessed == "yes" && letter2Guessed == "yes" && letter3Guessed == "yes" && letter4Guessed == "yes" && letter5Guessed == "yes") {
+    document.getElementById('scoreNotification').innerHTML = "You win!";
+}
 
 }
